c44f401957b9ba27d76affa1e877bcff3f9e02e1
Alex <Retho@yandex.ru>
2022-01-04 17:56:58 +0300

    fixed markup

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -18,8 +18,8 @@ let Skills = () => {
 //   }, [timer]) 
   return (
     <div className="blankPage">
-      <p>This webpage is not available.
-         We are sorry for this inconvenience</p>
+      <p>This webpage is not<br/> available.<br/>
+         We are sorry for this<br/> inconvenience</p>
       <img src={Cat}></img>
     </div>
   )
