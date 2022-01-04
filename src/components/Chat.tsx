6c74e5e11142f19ffd0c1b85873f22fa6a8cc637
Alex <Retho@yandex.ru>
2022-01-04 17:59:14 +0300

    fixed markup

---
+++ src/components/Chat.tsx
@@ -0,0 +1,14 @@
+import Cat from '../img/cat.png';
+
+function Chat() {
+  return (
+    <div className="blankPage">
+    <p>This webpage is not available.
+       We are sorry for this inconvenience</p>
+    <img src={Cat}></img>
+  </div>
+  )
+}
+
+
+export default Chat;
---
+++ src/components/Donate.tsx
@@ -0,0 +1,13 @@
+import Cat from '../img/cat.png';
+
+function Donate() {
+  return (
+    <div className="blankPage">
+      <p>This webpage is not available.
+         We are sorry for this inconvenience</p>
+      <img src={Cat}></img>
+    </div>
+  )
+}
+
+export default Donate;
