e2df5520879fb863321a8849add04a6959f7ca0c
Alex <a.malihin@it-justice.com>
2022-04-19 15:58:34 +0300

    test commit

--- src/components/Donate.tsx
+++ src/components/Donate.tsx
@@ -4,7 +4,7 @@ function Donate() {
   return (
     <div className="blankPage">
       <p>This webpage is not<br/> available.<br/>
-         We are sorry for this<br/> inconvenience</p>
+         We are sorry for this<br/> incnvenience</p>
       <img src={Cat}></img>
     </div>
   )
