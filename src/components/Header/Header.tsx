9d9319ea19dd7daa83a7ec91f29250afaf502da1
AlexMalikhin <a.malihin@it-justice.com>
2022-08-02 23:04:32 +0300

    Settin....

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -25,7 +25,7 @@ const Header: FC = () => {
 
   return (
     <S.Container>
-      geggefwfqfwefe
+      geggefwfqfwefeсйу
       dwdwfwefwefedwdwd
       <S.PagesLinks>
         {/*  <li><NavLink className='links' to='/Skills'>Skills</NavLink></li>*/}
